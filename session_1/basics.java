03be4e8d59650d334cd230415ea91f29f9e00f19
behdadjahad <behdadja@gmail.com>
2023-11-12 23:47:35 +0330

    first session code added.

---
+++ session_1/basics.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("tedad ra vared konid:");
+        int num = scan.nextInt();
+        String[] names = new String[num];
+        for (int i = 0 ; i < num ; i++) {
+            System.out.println("nam ra vared konid:");
+            names[i] = scanner.nextLine();
+        }
+
+        for (int i = 0 ; i < num ; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
