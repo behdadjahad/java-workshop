0324d7e9b95ce9972dc0bd0aa253c6aa50740f97
behdadjahad <behdadja@gmail.com>
2023-11-14 01:22:01 +0330

    fixing session 1 file

--- session_1/Basics.java
+++ session_1/Basics.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-public class Main {
+public class Basics {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Scanner scanner = new Scanner(System.in);
